564555d7a160a2aa3428da668f073ef8d78703d0
Owen Searfoss <owen@datura.io>
2018-06-19 17:32:26 -0700

    Initial work on file handling.

---
+++ hash.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+	// TODO can the 'flags' package be used in lieu of os here?
+	if len(os.Args) < 2 {
+		// ensure that a filename's been provided
+		fmt.Println("An input filename must be specified before a hash can be created.")
+		os.Exit(1)
+	}
+
+	// use Stat in lieu of Open here 'cause we'll need to know if
+	// we're operating on a file or directory
+	fs, serr := os.Stat(os.Args[1])
+	if serr != nil {
+		fmt.Println(serr)
+		os.Exit(1)
+	} else if fs.IsDir() {
+		// the operation was a success but we were provided a directory.
+		// throw an error for now; later we can handle this explicitly
+		fmt.Println("Can't operate on a directory. Please specify a specific file.")
+		os.Exit(1)
+	}
+
+	fmt.Printf("%+v\n",fs)
+}
